d3a68dfa487ebd16d89e12c6f626e07c6a58d7ea
Pasquale Rocchio <patrocc6@gmail.com>
2017-04-30 03:00:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  return false if number <= 1
+  (2...number).to_a.each do |divisor|
+    if number % divisor == 0
+      return false
+    end
+  end
+  return true
+end
